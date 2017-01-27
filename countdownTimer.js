acc03162c5c74d1afb5bbb931812d6dbd97ffc29
Cihan <cihannisan@gmail.com>
2017-01-26 16:53:43 -0800

    Add setInternal method to run constantly

--- countdownTimer.js
+++ countdownTimer.js
@@ -1,18 +1,23 @@
-// target date formats (1-26-2017) , (Jan 26, 2017) , (1/26/2017) )
+// Target date input formats (1-26-2017) , (Jan 26, 2017) , (1/26/2017) or (1 26 2017))
 
 
-function calculateRemainingTime(targetDate) {
+function countdownTimer(targetDate) {
   
-  var timeDifference = Date.parse(targetDate) - Date.now();
+  var remainingTime = Date.parse(targetDate) - Date.now();
+  var seconds = parseInt((remainingTime / 1000) % 60);
+  var minutes = parseInt((remainingTime / (1000 * 60)) % 60);
+  var hours = parseInt((remainingTime / (1000 * 60 * 60)) % 24);
+  var days = parseInt(((remainingTime) / (1000 * 60 * 60 * 24)) % 365);
   
-  var seconds = parseInt((timeDifference / 1000) % 60);
-  var minutes = parseInt((timeDifference / (1000 * 60)) % 60);
-  var hours = parseInt((timeDifference / (1000 * 60 * 60)) % 24);
-  var days = parseInt(((timeDifference) / (1000 * 60 * 60 * 24)) % 365);
-  var timeRemaining = {'seconds':seconds, 'minutes':minutes, 'hours':hours, 'days':days};
-  
-  if(targetDate === undefined) {
-    return "Please enter a valid time";
+  function startTimer() {
+    
+    if(targetDate === undefined) {
+      return "Please enter a valid time";
+    }
+    //console.log(seconds);
   }
-  return timeRemaining;
+  
+  //setInterval(startCountingDown,1000);
 }
+
+

b7d207d12d1d105d6431899627795b2538e944dc
Cihan <cihannisan@gmail.com>
2017-01-27 08:46:49 -0800

    Add helper functions to display timer on html

--- countdownTimer.js
+++ countdownTimer.js
@@ -1,25 +1,50 @@
-function countDownTimer(targetDate) {
-  
-  function startTimer() {
-    
-    var remainingTime = Date.parse(targetDate) - Date.now();
-    var seconds = parseInt((remainingTime / 1000) % 60);
-    var minutes = parseInt((remainingTime / (1000 * 60)) % 60);
-    var hours = parseInt((remainingTime / (1000 * 60 * 60)) % 24);
-    var days = parseInt(((remainingTime) / (1000 * 60 * 60 * 24)) % 365);
-    
-    if(targetDate === undefined) {
-      return "Please enter a valid time";
-    }
-    
-    if(remainingTime <= 0) {
-      clearInterval(counter);
-      return 'Time is up!';
-      
-    }
-    console.log(seconds,remainingTime);
+function countdownTimer(endTime) {
+  var counter;
+  var remainingTime;
+  var seconds;
+  var minutes;
+  var hours;
+  var days;
+
+  if(!endTime) {
+    return;
   }
-  
-  var counter = setInterval(startTimer,1000);
+
+  function displayTimer() {
+    var displayDay = document.getElementsByClassName('days')[0];
+    var displayHour = document.getElementsByClassName('hours')[0];
+    var displayMinutes = document.getElementsByClassName('minutes')[0];
+    var displaySecond = document.getElementsByClassName('seconds')[0];
+
+    function startTimer() {
+      remainingTime = Date.parse(endTime) - Date.now();
+      seconds = parseInt((remainingTime / 1000) % 60);
+      minutes = parseInt((remainingTime / (1000 * 60)) % 60);
+      hours = parseInt((remainingTime / (1000 * 60 * 60)) % 24);
+      days = parseInt(((remainingTime) / (1000 * 60 * 60 * 24)) % 365);
+   
+      displayDay.innerHTML = days;
+      displayHour.innerHTML = hours;
+      displayMinutes.innerHTML = minutes;
+      displaySecond.innerHTML = seconds;
+      
+      if(remainingTime <= 0) {
+        clearInterval(counter);
+      }
+    }
+    counter = setInterval(startTimer,1000);
+  }
+  displayTimer();
 }
 
+
+function getDeadline() {
+  var deadline = document.getElementsByClassName('date-input')[0].value;
+
+  countdownTimer(deadline);
+  resetForm();                        
+}
+
+function resetForm() {  
+  document.getElementsByClassName("my-form")[0].reset();
+}

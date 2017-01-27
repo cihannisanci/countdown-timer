6ff691ad03ebd084ab3c6bce5c3d308a623d96f3
Cihan <cihannisan@gmail.com>
2017-01-26 17:09:16 -0800

    Return a message when time is up

--- countdownTimer.js
+++ countdownTimer.js
@@ -1,23 +1,25 @@
-// Target date input formats (1-26-2017) , (Jan 26, 2017) , (1/26/2017) or (1 26 2017))
-
-
-function countdownTimer(targetDate) {
-  
-  var remainingTime = Date.parse(targetDate) - Date.now();
-  var seconds = parseInt((remainingTime / 1000) % 60);
-  var minutes = parseInt((remainingTime / (1000 * 60)) % 60);
-  var hours = parseInt((remainingTime / (1000 * 60 * 60)) % 24);
-  var days = parseInt(((remainingTime) / (1000 * 60 * 60 * 24)) % 365);
+function countDownTimer(targetDate) {
   
   function startTimer() {
+    
+    var remainingTime = Date.parse(targetDate) - Date.now();
+    var seconds = parseInt((remainingTime / 1000) % 60);
+    var minutes = parseInt((remainingTime / (1000 * 60)) % 60);
+    var hours = parseInt((remainingTime / (1000 * 60 * 60)) % 24);
+    var days = parseInt(((remainingTime) / (1000 * 60 * 60 * 24)) % 365);
     
     if(targetDate === undefined) {
       return "Please enter a valid time";
     }
-    //console.log(seconds);
+    
+    if(remainingTime <= 0) {
+      clearInterval(counter);
+      return 'Time is up!';
+      
+    }
+    console.log(seconds,remainingTime);
   }
   
-  //setInterval(startCountingDown,1000);
+  var counter = setInterval(startTimer,1000);
 }
-
 
